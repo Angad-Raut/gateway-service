29d5f12fff9c3dd9f76b30b97e57ab402089ccae
Angad-Raut <85058287+Angad-Raut@users.noreply.github.com>
2024-09-21 17:04:58 +0530

    API Gateway chabges commited

--- src/main/java/com/projectx/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/projectx/gateway_service/GatewayServiceApplication.java
@@ -6,7 +6,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.converter.json.AbstractJsonHttpMessageConverter.DEFAULT_CHARSET;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -22,7 +31,22 @@ public class GatewayServiceApplication extends SpringBootServletInitializer {
 	}
 
 	@Bean
-	public ServerCodecConfigurer serverCodecConfigurer() {
-		return ServerCodecConfigurer.create();
+	public RestTemplate restTemplate() {
+		final RestTemplate restTemplate = new RestTemplate();
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setSupportedMediaTypes(setMediaTypes());
+		messageConverters.add(converter);
+		restTemplate.setMessageConverters(messageConverters);
+		return restTemplate;
+	}
+
+	private List<MediaType> setMediaTypes() {
+			List<MediaType> types = Arrays.asList(
+					new MediaType("text", "html", DEFAULT_CHARSET),
+					new MediaType("application", "json", DEFAULT_CHARSET),
+					new MediaType("application", "*+json", DEFAULT_CHARSET)
+			);
+			return types;
 	}
 }
---
+++ src/main/java/com/projectx/gateway_service/configs/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.projectx.gateway_service.configs;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.jwt.secret}")
+    private String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init(){
+        byte[] keyBytes= Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return this.getAllClaimsFromToken(token).getExpiration().before(new Date());
+    }
+
+    public boolean isInvalid(String token) {
+        return this.isTokenExpired(token);
+    }
+}
